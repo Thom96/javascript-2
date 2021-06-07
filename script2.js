3746f2134810f492d3fca5b6e7923f8d7ba37a05
Thom96 <43645360+Thom96@users.noreply.github.com>
2021-06-07 19:05:33 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,24 @@
+const age = (19);
+const isFemale = false;
+const driverStatus = 'bobbbbb';
+
+    if (age <= 18){
+        console.log("Je mag niet naar binnen, je bent te jong")
+    }
+    else {
+        console.log("Kom verder!")
+    }
+
+    if (isFemale == true){
+        console.log("Welkom op de Ladies Night!")
+    }
+    else {
+        console.log("Sorry, vanavond is het Ladies Night.")
+    }
+
+    if (driverStatus == 'bob'){
+        console.log("Je mag nog rijden!")
+    }
+    else {
+        console.log("Je mag helaas niet meer rijden.")
+    }
